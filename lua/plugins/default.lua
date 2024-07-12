76daf0b093f7c3a250f59ef576b77d10102b7e07
Samuel Cobb <sjcobb2003@gmail.com>
2024-07-12 21:15:59 +0100

    update plugins

--- lua/plugins/default.lua
+++ lua/plugins/default.lua
@@ -9,24 +9,40 @@ return {
   },
 
   -- add telescope-fzf-native
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
-  },
+  -- {
+  --   "telescope.nvim",
+  --   dependencies = {
+  --     "nvim-telescope/telescope-fzf-native.nvim",
+  --     build = "make",
+  --     config = function()
+  --       require("telescope").load_extension("fzf")
+  --     end,
+  --   },
+  -- },
+  --
+  -- UI
+  { import = "lazyvim.plugins.extras.ui.alpha" },
 
+  -- Testing / Debugging
   { import = "lazyvim.plugins.extras.dap.core" },
+  { import = "lazyvim.plugins.extras.test.core" },
+
+  -- Lang
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.nix" },
+  { import = "lazyvim.plugins.extras.lang.php" },
+  { import = "lazyvim.plugins.extras.lang.svelte" },
+  { import = "lazyvim.plugins.extras.lang.angular" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.tailwind" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.util.dot" },
+
+  -- Telescope
+  { import = "lazyvim.plugins.extras.editor.telescope" },
+
+  -- Copilot
   { import = "lazyvim.plugins.extras.coding.copilot" },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,14 +2,17 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     opts = {
-      layout_config = {
-        horizontal = {
-          prompt_position = "top",
-        },
-        vertical = {
-          mirror = false,
-        },
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
       },
+    },
+    keys = {
+      -- disable the keymap to grep files
+      { "<leader>/", false },
+      -- change a keymap
+      { "<leader>fw", "<cmd>Telescope live_grep", "Find Words" },
     },
   },
 }
