6256e85fc326095132ea886b6348705524b478b3
Samuel Cobb <sjcobb2003@gmail.com>
2024-07-13 21:50:19 +0100

    do not use angualr lang server yet

--- lua/plugins/default.lua
+++ lua/plugins/default.lua
@@ -31,7 +31,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.nix" },
   { import = "lazyvim.plugins.extras.lang.php" },
   { import = "lazyvim.plugins.extras.lang.svelte" },
-  { import = "lazyvim.plugins.extras.lang.angular" },
+  -- { import = "lazyvim.plugins.extras.lang.angular" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.tailwind" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
