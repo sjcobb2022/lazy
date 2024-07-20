b11245020397f3b3dc45ea3ed0ad41387a8d6459
Samuel Cobb <sjcobb2003@gmail.com>
2024-07-20 23:33:53 +0100

    remove news

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,8 +26,9 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  -- install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = false, notify = false }, -- automatically check for plugin updates
+  news = { lazy = false },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true, notify = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
